cdf7e2efae6b11d69f7a40038f10b14b6545ceb1
luismarquez96c <luismarquez96c@gmail.com>
2023-11-20 13:19:29 -0600

    Logro: Creación del proyecto

---
+++ src/main/java/net/luismarquez/projects/IntroSpringDataJpa/IntroSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package net.luismarquez.projects.IntroSpringDataJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroSpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/luismarquez/projects/IntroSpringDataJpa/IntroSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.luismarquez.projects.IntroSpringDataJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
